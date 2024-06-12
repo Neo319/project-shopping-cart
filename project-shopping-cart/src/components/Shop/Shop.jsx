171df84373b9c22fa47e9311e9b19c5e4b0af5df
CastorDisaster <alexmnevins@gmail.com>
2024-06-12 10:33:58 -0400

    resolve issues in Shop after merge

--- project-shopping-cart/src/components/Shop/Shop.jsx
+++ project-shopping-cart/src/components/Shop/Shop.jsx
@@ -2,56 +2,21 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./Shop.module.css";
 
-export default function Shop() {
-  const [cart, setCart] = useState({});
-
-  const mockProducts = [
-    {
-      id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating: { rate: 3.9, count: 120 },
-    },
-    {
-      id: 2,
-      title: "Mens Casual Premium Slim Fit T-Shirts ",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
-      image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      rating: { rate: 4.1, count: 259 },
-    },
-  ];
-
-  const products = mockProducts;
-
-  function handleCartIncrease(item) {
-    if (Object.hasOwnProperty.call(cart, item.id)) {
-      const itemId = cart[item.id];
-      const newCart = { ...cart, [itemId]: cart[itemId]++ };
-      setCart(newCart);
-    }
-  }
-
-  function handleCartDecrease(item) {
-    if (Object.hasOwnProperty.call(cart, item.id)) {
-      const itemId = cart[item.id];
-      const newCart = { ...cart, [itemId]: cart[itemId]-- };
-      setCart(newCart);
-    }
-  }
-
-  // eslint-disable-next-line react/prop-types
+export default function Shop({
+  cart,
+  products,
+  handleCartIncrease,
+  handleCartDecrease,
+}) {
   const Card = ({ item, index }) => {
     const shortenedTitle = item.title.slice(0, 20) + " ... ";
     const shortenedDesc = item.description.slice(0, 100) + " ... ";
     const cartQuantity = cart[item.id];
+
+    Card.propTypes = {
+      item: PropTypes.string.isRequired,
+      index: PropTypes.number.isRequired,
+    };
 
     return (
       <div className={styles.card}>
