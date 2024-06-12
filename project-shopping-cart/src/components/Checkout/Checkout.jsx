4754f8039f74fc5fe527583039fa22ab3c4f34d2
CastorDisaster <alexmnevins@gmail.com>
2024-06-12 10:53:43 -0400

    add empty cart screen to checkout

--- project-shopping-cart/src/components/Checkout/Checkout.jsx
+++ project-shopping-cart/src/components/Checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import styles from "./Checkout.module.css";
+import { Link } from "react-router-dom";
 
 export default function Checkout({ cart, products, deleteCartItem }) {
   function CartList({ item }) {
@@ -27,20 +28,37 @@ export default function Checkout({ cart, products, deleteCartItem }) {
 
   //-----------------------------------------
   //TODO: add a checkout subtotal that adds up all prices and a mock confirm button!
+  //add a 'cart empty' screen
   return (
     <div className={styles.checkoutPage}>
       <h1>Hello from Checkout page!!</h1>
       <h1>Your Cart: </h1>
 
-      <ul>
-        {Object.keys(cart).map((key) => {
-          return (
-            <div key={key}>
-              <CartList item={key} />
-            </div>
-          );
-        })}
-      </ul>
+      {/* if there are any items in cart */}
+      {Object.keys(cart).length ? (
+        <ul>
+          {Object.keys(cart).map((key) => {
+            return (
+              <div key={key}>
+                <CartList item={key} />
+              </div>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className={styles.emptyCart}>
+          <img
+            width="64"
+            height="64"
+            src="https://img.icons8.com/external-lylac-kerismaker/64/external-Empty-delivery-lylac-kerismaker.png"
+            alt="external-Empty-delivery-lylac-kerismaker"
+          />
+          <span>Your cart is currently empty... </span>
+          <p>
+            Go to <Link to="/shop">the shop page</Link> to browse items!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
