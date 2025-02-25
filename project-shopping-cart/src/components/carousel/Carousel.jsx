9d11ef2b88449f3498181ab5e63bdbcc9501b39a
CastorDisaster <alexmnevins@gmail.com>
2025-02-25 15:16:44 -0500

    create react state for sliding carousel

--- project-shopping-cart/src/components/carousel/Carousel.jsx
+++ project-shopping-cart/src/components/carousel/Carousel.jsx
@@ -1,24 +1,28 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import "../../styles/Carousel.css";
 
 function Carousel({ products }) {
-  console.log("products:", products);
+  const [index, setIndex] = useState(0);
+
+  setInterval(() => {
+    if (index >= products.length - 1) {
+      setIndex(0);
+    } else setIndex(index + 1);
+  }, 2000);
 
   return (
     <>
       <div className="carousel">
-        {products.map((product) => {
-          return (
-            <div key={"carouselKey" + product.id}>
-              <img
-                src={product.image}
-                alt="error finding product image"
-                width="200px"
-              />
-              <h2>{product.title}</h2>
-            </div>
-          );
-        })}
+        {/* render the product from index */}
+        <div>
+          <img
+            src={products[index].image}
+            alt="error finding product image"
+            width="200px"
+          />
+          <h2>{products[index].title}</h2>
+        </div>
       </div>
     </>
   );
@@ -29,3 +33,18 @@ Carousel.propTypes = {
 };
 
 export default Carousel;
+
+{
+  /* {products.map((product) => {
+          return (
+            <div key={"carouselKey" + product.id}>
+              <img
+                src={product.image}
+                alt="error finding product image"
+                width="200px"
+              />
+              <h2>{product.title}</h2>
+            </div>
+          );
+        })} */
+}
